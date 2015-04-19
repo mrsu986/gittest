2c7132c1f6e01924a65ca7cc85c8d13d21ac2a79
mrsu986 <likeplusit702@gmail.com>
2015-04-19 23:15:26 +0900

    skype

--- src/gittest/start.java
+++ src/gittest/start.java
@@ -7,7 +7,8 @@ public class start {
 		System.out.printf("Hello World");
 		System.out.printf("Hello World 2nd season");
 		System.out.printf("Hello World 3rd season");
-
+		System.out.printf("Hello World 4th season");
+		System.out.printf("Hello World 5th season");
 
 
 
